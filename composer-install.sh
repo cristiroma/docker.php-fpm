677c02870e2b45a53310a3d7b3fdbede8e078430
Cristian Romanescu <cristian@romanescu.ro>
2020-11-07 01:41:44 +0200

    Downgrade to composer 1.x

--- composer-install.sh
+++ composer-install.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
-
-EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+VERSION="1.10.17"
+EXPECTED_CHECKSUM="$(wget -q -O - https://getcomposer.org/download/${VERSION}/composer.phar.sha256sum | cut -d' ' -f1)"
+php -r "copy('https://getcomposer.org/download/${VERSION}/composer.phar', 'composer');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha256', 'composer');")"
 
 if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
 then
     >&2 echo 'ERROR: Invalid installer checksum'
-    rm composer-setup.php
     exit 1
 fi
-
-php composer-setup.php --quiet
-RESULT=$?
-rm composer-setup.php
-mv composer.phar /usr/local/bin/composer
-chmod +x /usr/local/bin/composer
-exit $RESULT
+chmod +x composer
+mv composer /usr/local/bin/composer
